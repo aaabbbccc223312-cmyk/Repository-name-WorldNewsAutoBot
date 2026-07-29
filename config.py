bab80a12bc18238350b16b835722d30e2974f657
olabodekola877 <olabodekola877@gmail.com>
2026-07-29 15:35:15 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,21 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+CHANNELS = [
+    channel.strip()
+    for channel in os.getenv(
+        "CHANNELS",
+        "@paisabase1,@aatgpay,@smrtwallet"
+    ).split(",")
+]
+
+CHECK_INTERVAL = int(os.getenv("CHECK_INTERVAL", "60"))
+
+DATABASE_PATH = os.getenv(
+    "DATABASE_PATH",
+    "database/news.db"
+)
