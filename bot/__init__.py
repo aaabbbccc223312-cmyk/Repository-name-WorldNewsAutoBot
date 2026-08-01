a18df1b5a890d147a7671f9ac55e8bcb296d6ec2
olabodekola877 <olabodekola877@gmail.com>
2026-08-01 15:15:40 -0700

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -3,6 +3,7 @@ Telegram Bot Package
 """
 
 from .handlers import start, check_join
+
 from .commands import (
     addchannel,
     removechannel,
@@ -11,22 +12,34 @@ from .commands import (
     channels,
     stats,
 )
+
 from .membership import (
-    get_missing_channels,
     has_joined_all,
 )
+
 from .keyboards import join_keyboard
 
+
 __all__ = [
+
     "start",
+
     "check_join",
+
     "addchannel",
+
     "removechannel",
+
     "pausechannel",
+
     "resumechannel",
+
     "channels",
+
     "stats",
-    "get_missing_channels",
+
     "has_joined_all",
+
     "join_keyboard",
+
 ]
